83df3315f120f5e9d49e1a9fe9043bd2a2256b3e
Yeison Ospitia <yeisonospitiavoodoo@gmail.com>
2023-12-27 15:37:08 -0500

    part1 sin esitlos

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+function Button({text, handleclick}) {
+  return (
+    <button type="button" onClick={handleclick}>{text}</button>
+  )
+}
+function Votacion({votos}) {
+  return (
+    <span>has {votos} votes</span>
+  )
+}
+
+function Title({text}) {
+  return (
+      <h1>{text}</h1>
+  )
+}
+
+function MejorVotacion({title = 'There are no registered votes', votos = 0}){
+  return (
+    <div>
+      <Title text={'Anecdote with more votes'}></Title>
+      <p>{title}</p>
+      <span>has {votos} votes</span>
+    </div>
+  )
+}
+
+const puntosiniciales = [
+  0,0,0,0,0,0,0,0
+]
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const [puntos, setPuntos] = useState(puntosiniciales)
+  const [masVotado, setMasVotado] = useState()
+
+  const funcionAleatorio = () => {
+    return () =>{
+      let numeroAleatorio = Math.floor(Math.random()*anecdotes.length);
+      setSelected(numeroAleatorio)
+    }
+  }
+  const funcionVotar = (selected) => {
+    return () =>{
+      let copyPuntos = [...puntos]
+      copyPuntos[selected] += 1
+      const numeroMayor = Math.max(...copyPuntos)
+      const posicion = copyPuntos.indexOf(numeroMayor);
+      setMasVotado(posicion)
+      setPuntos(copyPuntos)
+    }
+  }
+  return (
+    <div>
+      <Title text={'Anecdote of the day'}></Title>
+      <p>{anecdotes[selected]}</p>   
+      <div>
+        <Votacion votos={puntos[selected]}></Votacion>
+      </div>
+      <div>
+        <Button handleclick={funcionVotar(selected)} text="vote"></Button>
+        <Button handleclick={funcionAleatorio()} text="next anecdote"></Button>
+      </div>
+      <MejorVotacion title={anecdotes[masVotado]} votos={puntos[masVotado]}></MejorVotacion>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react"
+
+function Header({text}){
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+function Parrafo({text}){
+  return (
+    <p>{text}</p>
+  )
+}
+
+function Button({text, handleclick}){
+  return (
+    <button onClick={handleclick}>{text}</button>
+  )
+}
+
+function StatisticLine({text, variable}){
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{variable}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+  if(all === 0){
+    return (
+      <>
+        <Parrafo text="No feedback given"></Parrafo>
+      </>
+    )
+  }else{
+    return (
+      <table>
+          <tbody>
+            <StatisticLine text="Good" variable={good}></StatisticLine>
+            <StatisticLine text="Neutral" variable={neutral}></StatisticLine>
+            <StatisticLine text="Bad" variable={bad}></StatisticLine>
+            <StatisticLine text="All" variable={all}></StatisticLine>
+            <StatisticLine text="Average" variable={average}></StatisticLine>
+            <StatisticLine text="Positive" variable={positive}></StatisticLine>
+        </tbody>
+      </table>
+    );
+  }
+};
+
+let acumulado = 0;
+
+function App() {
+  // Guarda los clics de cada boton en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const FuncionButton = (variable, modificador, valor) => {
+    return () => {
+      let total = all+1;
+      modificador(variable+1)
+      acumulado = acumulado+(valor)
+      setAverage(((acumulado)/(total)))
+      setAll(total)
+      valor > 0 ? setPositive((((good+valor)/(total))*100).toFixed(1)+ "%") : setPositive((((good)/(total))*100).toFixed(1)+ "%")
+    }
+  }
+  return (
+    <>
+      <Header text="give feedback"></Header>
+      <Button text="Good" handleclick={FuncionButton(good,setGood,1)}></Button>
+      <Button text="Neutral" handleclick={FuncionButton(neutral,setNeutral,0)}></Button>
+      <Button text="Bad" handleclick={FuncionButton(bad,setBad,-1)}></Button>
+      <Header text="Statistics"></Header>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average.toFixed(1)} positive={positive} />
+    </>
+  )
+}
+
+export default App
